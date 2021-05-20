103a590fa31ca99d8ea4ab1c544811d11e467f9a
nihar <vankadari.vikas@orchids.edu.in>
2021-05-20 08:30:36 +0530

    ui changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,26 @@
-import { combineReducers } from 'redux';
+import {
+  combineReducers,
+  createStore,
+  applyMiddleware,
+  compose,
+} from 'redux';
+import thunk from 'redux-thunk';
 import AuthReduser from './auth';
 
-export default combineReducers({ AuthReduser });
+const reducers = combineReducers({ AuthReduser });
+
+const composeEnhancers = typeof window === 'object'
+&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
+
+const store = createStore(
+  reducers,
+  enhancer,
+);
+
+export default store;
